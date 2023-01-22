c04c3c4447c7e1feb6fb46a856350f14fd773770
jafetsinke <jafet.sinke@student.hu.nl>
2023-01-23 00:19:58 +0100

    swap session if statement

--- spotify-stats/pages/index.tsx
+++ spotify-stats/pages/index.tsx
@@ -6,22 +6,21 @@ import Top from '@/components/Top';
 export default function Home() {
   const { data: session } = useSession()
 
-  // TODO suggest to swap if and else branches to avoid 'double negation'
-  if (!session) {
-    return (
-      <>
-        <h1>Welcome to Spotify Stats</h1>
-        <p>This is a simple tool made by Jafet Sinke to display your top tracks &amp; artists</p>
-        <strong>To get started, please sign in using your Spotify account in the top right corner.</strong>
-      </>
-    )
-  } else {
+  if (session) {
     return (
       <>
         <h1>Welcome to Spotify Stats</h1>
         <p>You&apos;re signed in as <strong><Link href="/profile">{session?.user?.email}</Link></strong> (click to show profile)</p>
         <Top type="tracks" />
         <Top type="artists" />
+      </>
+    )
+  } else {
+    return (
+      <>
+        <h1>Welcome to Spotify Stats</h1>
+        <p>This is a simple tool made by Jafet Sinke to display your top tracks &amp; artists</p>
+        <strong>To get started, please sign in using your Spotify account in the top right corner.</strong>
       </>
     )
   }
