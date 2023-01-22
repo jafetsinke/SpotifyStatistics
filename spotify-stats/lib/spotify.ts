31c6e1c8d36428ac2c3856cd7e6ef5209cce78ac
jafetsinke <jafet.sinke@student.hu.nl>
2023-01-23 00:31:12 +0100

    Fix access token always refreshing even when not needed
    Now only refreshes when token expired

--- spotify-stats/lib/spotify.ts
+++ spotify-stats/lib/spotify.ts
@@ -5,7 +5,7 @@ const CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
 const AUTHORIZATION = Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64');
 
 const getAccessToken = async (token: JWT) => {
-  if (token.accessTokenExpires < Date.now()) {
+  if (token.accessTokenExpires > Date.now()) {
     return token.accessToken;
   } else {
     const response = await fetch("https://accounts.spotify.com/api/token", {
@@ -19,33 +19,34 @@ const getAccessToken = async (token: JWT) => {
         refresh_token: token.refreshToken,
       }),
     });
-    return response.json();
+    const res = await response.json();
+    return res.access_token;
   }  
 };
 
 export const getUsersPlaylists = async (token: JWT) => {
-  const {access_token} = await getAccessToken(token);
+  const accessToken = await getAccessToken(token);
   return fetch("https://api.spotify.com/v1/me/playlists", {
     headers: {
-      Authorization: `Bearer ${access_token}`,
+      Authorization: `Bearer ${accessToken}`,
     },
   });
 };
 
 export const getUsersMostListened = async (type: "artists" | "tracks", token: JWT, limit: number, offset: number = 0, time_range: SpotifyTimeRange = "medium_term") => {
-  const {access_token} = await getAccessToken(token);
+  const accessToken = await getAccessToken(token);
   return fetch(`https://api.spotify.com/v1/me/top/${type}?limit=${limit}&offset=${offset}&time_range=${time_range}`, {
     headers: {
-      Authorization: `Bearer ${access_token}`,
+      Authorization: `Bearer ${accessToken}`,
     },
   });
 };
 
 export const getOwnSpotifyProfile = async (token: JWT) => {
-  const {access_token} = await getAccessToken(token);
+  const accessToken = await getAccessToken(token);
   return fetch("https://api.spotify.com/v1/me/", {
     headers: {
-      Authorization: `Bearer ${access_token}`,
+      Authorization: `Bearer ${accessToken}`,
     },
   });
 };
