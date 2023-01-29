621aa8ce20d9564005839e767ec0db63e36c3f73
jafetsinke <jafet.sinke@student.hu.nl>
2023-01-29 16:43:26 +0100

    Fix limit/offset params not working

--- spotify-stats/pages/api/spotify/me/top/[type].ts
+++ spotify-stats/pages/api/spotify/me/top/[type].ts
@@ -12,16 +12,16 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     return res.status(401).json({error: 'Unauthorized'});
   }
   //TODO: type must be artist/tracks enum
-  const {type, limitStr = '10', offsetStr = '0', time_range = 'medium_term'} = req.query;
+  const {type, limit = '10', offset = '0', time_range = 'medium_term'} = req.query;
 
   if (type !== 'artists' && type !== 'tracks') {
     return res.status(400).json({error: 'Invalid type'});
   }
 
-  const limit = toInt(limitStr);
-  const offset = toInt(offsetStr);
+  const limitInt = toInt(limit);
+  const offsetInt = toInt(offset);
 
-  const response = await getUsersMostListened(type, session.token, limit, offset, time_range as SpotifyTimeRange);
+  const response = await getUsersMostListened(type, session.token, limitInt, offsetInt, time_range as SpotifyTimeRange);
   
   if (response.status !== 200) {
     return res.status(response.status).json({error: 'Failed to get top artists from Spotify: ' + response.statusText});
