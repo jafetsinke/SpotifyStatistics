eee8f29aeee33703944b4be9c6061239f94b19db
jafetsinke <jafet.sinke@student.hu.nl>
2023-02-14 12:27:27 +0100

    median calculation fixed
    returned NaN/null, now fixed

--- spotify-stats/lib/utils.ts
+++ spotify-stats/lib/utils.ts
@@ -6,15 +6,15 @@ export function averageOfKeyInArray(key: string, array: any[]) {
 
 // takes an array of objects and returns the median of the key
 export function medianOfKeyInArray(key: string, array: any[]) {
-  const sorted = array.sort((a, b) => a[key] - b[key]);
+  const values = array.map((item) => item[key]);
+  const sorted = values.sort((a, b) => a - b);
   const middle = Math.floor(sorted.length / 2);
 
   if (sorted.length % 2) {
     return sorted[middle];
-  }
-
-  return (sorted[middle - 1] + sorted[middle]) / 2;
-  
+  } else {
+    return (sorted[middle - 1] + sorted[middle]) / 2;
+  }  
 }
 
 // returns a random number between min and max, not rounded
